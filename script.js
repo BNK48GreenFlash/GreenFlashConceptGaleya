325a7eff451577919b8884f98de73fe50944832b
BNK48GreenFlash <kalosrsxy@gmail.com>
2025-02-03 21:04:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("tweetButton").addEventListener("click", tweetMessage);
+});
+
+function tweetMessage() {
+    const message = "Galeya GreenFlash\n#GaleyaBNK48\n#GreenFlashConceptDay1";
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
+    window.open(tweetUrl, '_blank');
+}
